8b67abdbdf44d117990ff80cf304c3070435f30f
dnettoRaw <me@dnetto.dev>
2022-06-03 19:53:25 +0200

    i dont now

--- src-tauri/src/menu-toolbar/mod.rs
+++ src-tauri/src/menu-toolbar/mod.rs
@@ -1,17 +1,5 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*  mod.rs                                                    #####           */
-/*                                                         ############       */
-/*  By: dnettoRaw <contact@dnetto.dev>                   ###          ###     */
-/*                                                      ##    ##  ##    ##    */
-/*  obs: put here the public module name                      ##  ##          */
-/*       exemple:                                                             */
-/*       pub mod my_super_function;                     ##    ##  ##   ##     */
-/*                                                       ###  ######  ###     */
-/*  Created: 2022/06/02 16:52:41 by dnettoRaw             #####    ####       */
-/*  Updated: 2022/06/02 16:54:05 by dnettoRaw                                 */
-/*                                                    https://dnetto.dev      */
-/* ************************************************************************** */
-
-
 pub mod menus;
+
+
+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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
+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
