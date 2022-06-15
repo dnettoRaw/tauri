716141948fcefd398447f2f94048079018f652aa
dnettoRaw <me@dnetto.dev>
2022-06-15 20:09:19 +0200

    clean git

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,7 +3,7 @@
 /*  ##   ## ##   ##   F: vite-env.d.ts                        */
 /*       ## ##                                                */
 /*                    C: 2022/06/15 16:15:34 by:dnettoRaw     */
-/*  ##   ## ##   ##   U: 2022/06/15 16:49:16 by:dnettoRaw     */
+/*  ##   ## ##   ##   U: 2022/06/15 20:08:52 by:dnettoRaw     */
 /*    ###########                                             */
 
 /// <reference types="vite/client" />
