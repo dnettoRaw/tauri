6e57c83dc78ce98d29a798e60fb15966e33e58fd
dnettoRaw <me@dnetto.dev>
2022-06-18 17:21:00 +0200

    testing relase workflow

--- src-tauri/src/build.rs
+++ src-tauri/src/build.rs
@@ -3,15 +3,17 @@
 /*  ##   ## ##   ##   F: build.rs                             */
 /*       ## ##                                                */
 /*                    C: 2022/06/15 16:11:41 by:dnettoRaw     */
-/*  ##   ## ##   ##   U: 2022/06/15 16:51:46 by:dnettoRaw     */
+/*  ##   ## ##   ##   U: 2022/06/18 17:19:19 by:dnettoRaw     */
 /*    ###########                                             */
 
 use tauri_build::{try_build, Attributes, WindowsAttributes};
 
 fn main() {
   if let Err(error) = try_build(
-    Attributes::new()
-      .windows_attributes(WindowsAttributes::new().window_icon_path("../icons/icon.ico")),
+    #[cfg(any(target_os = "linux", target_os = "macos"))]
+    Attributes::new().windows_attributes(WindowsAttributes::new().window_icon_path("../icons/icon.ico")),
+    #[cfg(target_os = "windows")]
+    // Attributes::new().windows_attributes(WindowsAttributes::new().window_icon_path("..\\icons\\icon.ico")),
   ) {
     panic!("error found during tauri-build: {:#?}", error);
   }
